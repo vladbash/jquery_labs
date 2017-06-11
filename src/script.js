fe31152b37b90c9fcafe9cced2c01264dd6d1b47
vladb95 <vladb95z-soft@yandex.ru>
2017-06-11 16:30:34 +0300

    completed the second task

--- src/script.js
+++ src/script.js
@@ -3,4 +3,28 @@ $(document).ready(function() {
         _.replace($("#paragraph1").text(), /(\w+){6}/g, function (word) {
         return "<span style='background-color: red; font-size: 30px;'>" + word + "</span>";
     }));
+
+    $("#button1").click(function () {
+        $("#paragraph2").css("background-color", "yellow");
+        $("#paragraph2").css("font-size", "48px");
+    });
+
+    $("#button1").dblclick(function () {
+        $("#paragraph2").removeAttr("style");
+    });
+
+    $("#orange-border-block").css("border", "4px solid orange");
+
+    $("#link").hover(function () {
+        $(this).css("color", "green");
+    },function () {
+        $(this).removeAttr("style");
+    });
+
+    $("#button2").click(function() {
+        $("#paragraph3").css("font-family", "Geneva, Arial, Helvetica, sans-serif");
+        $("#paragraph3").css("font-size", "24px");
+        $("#paragraph4").css("font-weight", "bold");
+        $("#paragraph4").css("border", "4px solid black");
+    });
 });
